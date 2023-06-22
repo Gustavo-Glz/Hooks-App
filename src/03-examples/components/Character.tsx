f76417d6755fd60df36e48e1433e7131398e86ca
Gustavo-Glz <gustavo.gonzalez.montalvo@gmail.com>
2023-06-22 14:27:15 -0600

    Agregando ejemplo de useLayoutEffect

--- src/03-examples/components/Character.tsx
+++ src/03-examples/components/Character.tsx
@@ -1,15 +1,28 @@
+import { useLayoutEffect, useRef, useState } from "react";
+
 interface Props {
   image: string;
   name: string;
 }
 
 export const Character: React.FC<Props> = ({ image, name }) => {
+  const hRef = useRef<HTMLHeadingElement>(null);
+  const [hSize, setHSize] = useState({width: 0, height: 0});
+  useLayoutEffect(() => {
+    const {width, height} = hRef.current.getBoundingClientRect();
+    setHSize({width, height})
+  }, [name]);
   return (
-    <div className="card" style={{ width: "20rem" }}>
-      <img className="card-img-top" src={image} alt={name} />
-      <div className="card-body">
-        <h3 className="card-title">{name}</h3>
+    <>
+      <div className="card" style={{ width: "20rem" }}>
+        <img className="card-img-top" src={image} alt={name} />
+        <div className="card-body">
+          <h3 ref={hRef} className="card-title">
+            {name}
+          </h3>
+        </div>
       </div>
-    </div>
+      <code className="mt-5">{JSON.stringify(hSize)}</code>
+    </>
   );
 };
--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 import "./index.css";
-import { FocusScreen } from "./04-useRef/FocusScreen";
+import { Layout } from "./05-useLayoutEffect/Layout"
+// import { FocusScreen } from "./04-useRef/FocusScreen";
 // import { MultipleCustomHooks } from "./03-examples/MultipleCustomHooks";
 // import { FormCustomHook } from "./02-useEffect/FormCustomHook";
 // import { SimpleForm } from "./useEffect/SimpleForm";
@@ -18,6 +19,7 @@ ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
     {/* <SimpleForm /> */}
     {/* <FormCustomHook /> */}
     {/* <MultipleCustomHooks /> */}
-    <FocusScreen />
+    {/* <FocusScreen /> */}
+    <Layout />
   </React.StrictMode>
 );
